4dc01ea9a6c3d3ff080a815678bed7d342c3c42f
Anas <17gcs23@meaec.edu.in>
2023-06-06 09:51:10 +0530

    QuestionList

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import questions from "../data/questions.json";
+import QuestionListItem from "../src/components/QuestionListItem";
 
 export default function Page() {
+
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
+      <FlatList
+        data={questions.items}
+        renderItem={({ item }) => <QuestionListItem question={item} />}
+      />
     </View>
   );
 }
@@ -14,21 +17,7 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
+    padding: 10,
+    backgroundColor: "white",
   },
 });
---
+++ src/components/QuestionListItem.js
@@ -0,0 +1,71 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+
+const QuestionListItem = ({ question }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.stats}>
+        {question.score} votes •{' '}
+        {question.is_answered && (
+          <Entypo name="check" size={12} color="limegreen" />
+        )}
+        {question.answer_count} answers • {question.view_count} views
+      </Text>
+      <Text style={styles.title}>{question.title}</Text>
+      <Text style={styles.body} numberOfLines={2}>
+        {question.body_markdown}
+      </Text>
+      <View style={styles.tags}>
+        {question.tags.map((tag) => (
+          <Text key={tag} style={styles.tag}>{tag}</Text>
+        ))}
+        <Text style={styles.time}>
+          asked {new Date(question.creation_date * 1000).toDateString()}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+
+    borderBottomWidth: 0.5,
+    borderColor: 'lightgray',
+  },
+  stats: {
+    fontSize: 12,
+  },
+  title: {
+    color: '#0063bf',
+    marginVertical: 5,
+  },
+  body: {
+    fontSize: 11,
+    color: 'dimgrey',
+    lineHeight: 15,
+  },
+  tags: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 5,
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  tag: {
+    backgroundColor: '#e1ecf4',
+    color: '#39739d',
+    padding: 5,
+    fontSize: 12,
+    borderRadius: 3,
+    overflow: 'hidden',
+  },
+  time: {
+    marginLeft: 'auto',
+    fontSize: 12,
+    color: '#6a737c',
+  },
+});
+
+export default QuestionListItem;
